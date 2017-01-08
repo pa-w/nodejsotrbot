7863141612a32ca4a69eb556ea8588cfb0f239e7
pa-w <paw@paw.mx>
2017-01-08 18:07:21 -0500

    NLP sequencing changes, it is smarter and now knows how to parse questions with possessives

--- modules/nlp.js
+++ modules/nlp.js
@@ -72,6 +72,9 @@ module.exports.parse = function (body) {
 				console.log ("\t" + y + " " +analysis.terms [x].pos [y]);
 			}
 			*/
+			if (analysis.terms [x-1] && analysis.terms [x-1].pos.Possessive) { 
+				analysis.terms [x].possessive = analysis.terms [x-1];
+			}
 			if (!isQuestion && analysis.terms [x].pos.Question) { 
 				isQuestion = true;
 				questions.push (analysis.terms [x]);
@@ -79,8 +82,15 @@ module.exports.parse = function (body) {
 			if (!responseTime && analysis.terms [x].pos.Verb && !analysis.terms [x].pos.Copula) {
 				responseTime = analysis.terms [x].tag;
 			}
-			if (analysis.terms [x].pos.Verb && (analysis.terms [x].pos.Infinitive || analysis.terms [x].pos.Gerund)) {
-				verbs.push (analysis.terms [x]);
+			if (analysis.terms [x].pos.Verb && (analysis.terms [x].pos.Infinitive || analysis.terms [x].pos.Gerund || analysis.terms [x].pos.Copula)) {
+				if (analysis.terms [x].pos.Copula) { 
+					var w = analysis.terms [x].normal.split (' ');
+					if (w.length > 1) {
+						verbs.push (nlp.verb (analysis.terms [x].normal.split (' ') [w.length - 1]));
+					}
+				} else {
+					verbs.push (analysis.terms [x]);
+				}
 			}
 			if (analysis.terms [x].pos.Person) { 
 				people.push (analysis.terms [x]);
@@ -93,16 +103,24 @@ module.exports.parse = function (body) {
 				dates.push (analysis.terms [x]);
 			}
 		}
-		var key_noun = nouns.length > 0 ? nouns [nouns.length - 1].normal : "";
-		if (verbs.length == 0) key_noun = nouns.map (function (x) { return x.normal; }).join (" ");
-		var key_verb = verbs.length > 0 ? nlp.verb (verbs [verbs.length - 1].normal).conjugate ().infinitive : "";
-		var key = key_verb + "_" + key_noun;
-		if (key == "_" && questions.length > 0) { 
-			key = questions [0].normal + "_" + (people.length > 0 ? people [0].normal : analysis.terms [analysis.terms.length - 1].normal);  
-		} else if (key == "_") {
-			key = analysis.terms [0].normal + "_" + analysis.terms [analysis.terms.length - 1].normal;
+		var key_noun = nouns.length > 0 ? (nouns [nouns.length -1].possessive ? nouns [nouns.length -1].possessive.normal + "_" + nouns [nouns.length -1].normal  : nouns [nouns.length - 1].normal) : "";
+		if (key_noun == "" && people.length > 0 ) { key_noun = people [people.length -1].normal; }
+		if (verbs.length == 0) key_noun = nouns.map (function (x) { return (x.possessive ? x.possessive.normal + "_" : "") + x.normal; }).join (" ");
+		var key_verb = verbs.length > 0 ? (verbs [verbs.length -1].possessive ? verbs [verbs.length -1].possessive.normal + "_" : "") + nlp.verb (verbs [verbs.length - 1].normal).conjugate ().infinitive : "";
+		var keys = [];
+		if (key_verb) keys.push (key_verb);
+		if (key_noun) keys.push (key_noun);
+		if (keys.length == 0 && questions.length > 0) { 
+			keys.push ((questions [0].expansion ? questions [0].expansion : questions [0].normal));
+			if (verbs.length > 0) { 
+				keys.push (verbs [0].normal);
+			}
+			keys.push ((people.length > 0 ? people [0].normal : analysis.terms [analysis.terms.length - 1].normal));  
+		} else if (keys.length == 0) {
+			keys.push (analysis.terms [0].normal)
+			keys.push (analysis.terms [analysis.terms.length - 1].normal);
 		}
-		terms.push ({"key": key, "question": isQuestion, "time": responseTime, "verbs": verbs, "people": people, "dates": dates, "nouns": nouns});
+		terms.push ({"key": keys.join ('_').toLowerCase (), "question": isQuestion, "time": responseTime, "verbs": verbs, "people": people, "dates": dates, "nouns": nouns});
 	}
 
 	return terms;
