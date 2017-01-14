0b8e37e769811953405cd7b5575f40337784e3d5
pa-w <paw@paw.mx>
2017-01-14 15:25:49 -0500

    modified commands so it takes the otr instance

--- modules/commands.js
+++ modules/commands.js
@@ -4,25 +4,26 @@ var fs = require ('fs');
 var EventEmitter = require('events').EventEmitter;
 module.exports = new EventEmitter ();
 module.commands = {};
-module.commands.say = function (what, conf, user, client) { 
-	client.send (stanza.Message.send (user.service, user.id, conf.sentences [what][user.locale ? user.locale : conf.default_locale]));
+module.commands.say = function (what, conf, user, client, otr) { 
+	otr.sendMsg (conf.sentences [what][user.locale ? user.locale : conf.default_locale]);
+	//client.send (stanza.Message.send (user.service, user.id, conf.sentences [what][user.locale ? user.locale : conf.default_locale]));
 }
-module.exports.exec = function (action, conf, user, client) { 
+module.exports.exec = function (action, conf, user, client, otr) { 
 	var cmds = action.split(";");
 	for (var cmd in cmds) { 
 		var w = cmds [cmd].split (" ");
 		if (module.commands [w [0]]) {
-			module.commands [w [0]] (w.slice (1).join (" "), conf, user, client);
+			module.commands [w [0]] (w.slice (1).join (" "), conf, user, client, otr);
 		}
 	}
 }
-module.exports.parse = function (file, cmd, user, client) { 
+module.exports.parse = function (file, cmd, user, client, otr) { 
 	try {
 		if (fs.statSync (__dirname + "/" + file)) {
 			try {
 				var r = JSON.parse (fs.readFileSync (__dirname + "/" + file));
 				if (r.actions && r.default_action && r.actions [r.default_action]) {
-					module.exports.exec (r.actions [r.default_action], r, user, client);
+					module.exports.exec (r.actions [r.default_action], r, user, client, otr);
 				} else {
 					log.info ("no default action");
 				}
--- modules/stanza/message.js
+++ modules/stanza/message.js
@@ -3,7 +3,13 @@ var EventEmitter = require('events').EventEmitter;
 var log = require ('../log');
 module.exports = new EventEmitter ();
 module.exports.parse = function (msg) { 
+	if (!msg) {
+		log.info ("NO MESSAGE RECEIVED!");
+		return;
+	}
+	log.info ("will parse");
 	var body = msg.getChildText ('body'), composing = msg.getChild ('composing'), paused = msg.getChild ('paused');
+	log.info (body + " " + composing + " " + paused);
 	if (body) { 
 		module.exports.emit ("message", msg.attrs, body);
 	}
