82dc29124bff6323050ca4b8b1d4d03bf3800938
pa-w <paw@paw.mx>
2017-04-12 18:15:03 -0400

    Connected bot events.. next up is to add RIVESCRIPT

--- bot.js
+++ bot.js
@@ -1,12 +1,17 @@
+var fs = require ('fs');
 var Bot = require ("./obj/bot");
+
+var privateKey = fs.readFileSync (process.env.PK).toString ();
 
 var b = new Bot ()
 
 b.protocol ("xmpp")
 	.username (process.env.JID)
 	.password (process.env.JID_PWD)
-	.layer ("otr")
+	.layer ("otr", {"privateKey": privateKey})
 	.on ("online", function () { console.log ("online"); })
 	.on ("subscribe", function (attrs) { console.log ("FRIEND REQUEST"); b.acceptFriendship (attrs.from); })
 	.on ("offline", function () { console.log ("offline"); })
-	.on ("buddy_add", function (buddy) { console.log ("ADDED BUDDY: " + buddy.username ()); });
+	.on ("buddy_add", function (buddy) { console.log ("ADDED BUDDY: " + buddy.username ()); })
+	.on ("error", function (msg) { console.log (msg); })
+	.on ("received_message", function (from, to, msg) { console.log ("RECEIVED MSG: " + msg); })
